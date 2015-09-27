beddbc3918753577e56c2b73e10275c6f6f7d8bb
Igor28 <ihorurbanskiy@ukr.net>
2015-09-27 21:28:13 +0300

    no message

--- Lesson1/src/mas/Mas.java
+++ Lesson1/src/mas/Mas.java
@@ -3,32 +3,37 @@ package mas;
 public class Mas {
 
 	public static void main(String[] args) {
-		int[] array=new int[]{5, -4, 10, 25,54};
-			
+		int[] array = new int[] { 5, -4, 10, 25, 54 };
+
 		arrayMax(array);
 		arrayMin(array);
-			 
-	        }
-	public static void arrayMax(int[] array){
+
+	}
+
+	public static void arrayMax(int[] array) {
 		int max = array[0];
-		for(int i=0; i< array.length; i++ ){
-			if (max<array[i]){
-				max=array[i];
+		for (int i = 0; i < array.length; i++) {
+			if (max < array[i]) {
+				max = array[i];
 			}
 		}
-		System.out.println("Максимальне значення "+ max);
+		System.out.println("Максимальне значення " + max);
 	}
-	public static void arrayMin(int[] array){
+
+	public static void arrayMin(int[] array) {
 		int min = array[0];
-		for(int i=0; i< array.length; i++ ){
-			if (min>array[i]){
-				min=array[i];
+		for (int i = 0; i < array.length; i++) {
+			if (min > array[i]) {
+				min = array[i];
 			}
 		}
-		System.out.println("Мінімальне значення "+ min);
+		System.out.println("Мінімальне значення " + min);
 	}
 	
-	
+	public static void arraysum(int[] array) {
+		int sum;
+		for (int i = 0; i < array.length; i++) {
+			
+		}
 	}
-
-
+}
