dab43c3ba17a0c77193e5c88f6a4e16dcab1bea3
Igor28 <ihorurbanskiy@ukr.net>
2015-09-27 21:24:06 +0300

    no message

---
+++ Lesson1/src/mas/Mas.java
@@ -0,0 +1,34 @@
+package mas;
+
+public class Mas {
+
+	public static void main(String[] args) {
+		int[] array=new int[]{5, -4, 10, 25,54};
+			
+		arrayMax(array);
+		arrayMin(array);
+			 
+	        }
+	public static void arrayMax(int[] array){
+		int max = array[0];
+		for(int i=0; i< array.length; i++ ){
+			if (max<array[i]){
+				max=array[i];
+			}
+		}
+		System.out.println("Максимальне значення "+ max);
+	}
+	public static void arrayMin(int[] array){
+		int min = array[0];
+		for(int i=0; i< array.length; i++ ){
+			if (min>array[i]){
+				min=array[i];
+			}
+		}
+		System.out.println("Мінімальне значення "+ min);
+	}
+	
+	
+	}
+
+
